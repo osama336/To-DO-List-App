06875e78001303f43a9ed5ad6a550963ef519c1b
osama336 <osamamoh2022@yandex.com>
2026-01-28 11:03:43 +0300

    Initial commi

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import "./ToDoList.css"; // optional - if you want to add styles
+import ToDoList from './ToDoList.jsx'
+function App() {
+   
+
+
+ 
+    
+  
+
+  return (
+    <ToDoList/>
+  )
+
+ }
+export default App
